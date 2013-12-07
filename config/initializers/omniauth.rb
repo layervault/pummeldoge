50a06d45d3fc10755d5aa3127c5539c2741bb894
Kelly Sutton <michael.k.sutton@gmail.com>
2013-12-07 16:16:51 -0500

    Souping up our status page. Throwing errors when we aren't configured correctly.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+raise "Please specify your LAYERVAULT_CLIENT_ID with `export LAYERVAULT_CLIENT_ID=MY_KEY" if ENV['LAYERVAULT_CLIENT_ID'].blank?
+raise "Please specify your LAYERVAULT_CLIENT_SECRET with `export LAYERVAULT_CLIENT_ID=MY_KEY" if ENV['LAYERVAULT_CLIENT_SECRET'].blank?
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :layervault,
     ENV['LAYERVAULT_CLIENT_ID'],
